10f047085c480cf8cdec9ac56867953c718f4543
Aritra Saha <arittrosaha@gmail.com>
2018-05-08 17:16:29 -0400

    Start creating Auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: {format: :json} do
+    resource :user, only: [:create, :index, :show, :update]
+    resource :session, only: [:create, :destroy]
+  end
+
+  get 'api/users/:id/follows', :to => 'api/users#follows_create'
+  get 'api/follows/:id', :to => 'api/users#follows_destroy'
+
+  root "static_pages#root"
 end
