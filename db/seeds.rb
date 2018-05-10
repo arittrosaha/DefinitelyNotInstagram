adc66450dcb63bbab211a2cbade5a7475047c172
Aritra Saha <arittrosaha@gmail.com>
2018-05-10 17:01:05 -0400

    Seed demoUser

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(
+  email: 'demoUser@gmail.com',
+  full_name: 'Demo User',
+  username: 'demoUser',
+  password: 'DemoUserAS'
+)
